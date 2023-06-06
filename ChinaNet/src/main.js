cc853b90ad166a6c49d3aa1803dbf6b489220edc
Li He <80284104+Li-8023@users.noreply.github.com>
2023-06-06 10:38:01 +0800

    update login page

--- ChinaNet/src/main.js
+++ ChinaNet/src/main.js
@@ -3,6 +3,8 @@ import router from './router'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import './style.css'
+import VueToast from 'vue-toast-notification'
+import 'vue-toast-notification/dist/theme-sugar.css'
 import App from './App.vue'
 
-createApp(App).use(ElementPlus).use(router).mount('#app')
+createApp(App).use(ElementPlus).use(VueToast).use(router).mount('#app')
--- ChinaNet/src/views/Home.vue
+++ ChinaNet/src/views/Home.vue
@@ -3,20 +3,19 @@
         <div class="background-image">
             <div class="window">
                 <div class="content">
-                    <h1>登录界面</h1>
+                    <h1 class="heading">吉安电信考勤登录界面</h1>
                     <div class="divider"></div>
                     <form @submit.prevent="login">
                         <div class="form-group">
-                            <!--<label for="username">Username:</label>-->
                             <input type="text" id="username" v-model="username" required class="input-field"
                                 placeholder="请输入用户名称" />
                         </div>
                         <div class="form-group">
-                            <!--<label for="password">Password:</label>-->
                             <input type="password" id="password" v-model="password" required class="input-field"
                                 placeholder="请输入密码" />
                         </div>
-                        <el-button type="success" round @click="goToNext">登录</el-button>
+                        <button class="custom-button" @click="goToNext">登录</button>
+                        <p v-if="showWarning" class="warning">请输入用户名或登录密码</p>
                     </form>
                 </div>
             </div>
@@ -26,33 +25,77 @@
 
 <script>
 export default {
-    data() {
-        return {
-            username: '',
-            password: '',
-        };
-    },
-    methods: {
-        goToNext() {
-            this.$router.push('/mainView');
+  data() {
+    return {
+      username: '',
+      password: '',
+      showWarning: false,
+    };
+  },
+  computed: {
+        isValidInput() {
+            return this.username && this.password; // Check if both fields are non-empty
         },
-        login() {
-            // Here you can implement the login functionality,
+    },
+  methods: {
+    goToNext() {
+      if (this.isValidInput) {
+                this.$router.push('/mainView');
+            }
+    },
+    login() {
+         // Here you can implement the login functionality,
             // such as sending the login request to a server or performing client-side validation.
             // You can access the entered username and password from this.username and this.password respectively.
+        if (!this.username || !this.password) {
+            // Check if either username or password is empty
+            this.showWarning = true; // Show warning message
+        } else {
+            // Both username and password are entered
+            this.showWarning = false; // Hide warning message
             console.log('Login request:', this.username, this.password);
-        },
+            // Proceed with login functionality
+        }
+        console.log('Login request:', this.username, this.password);
     },
+  },
 };
 </script>
 
 <style>
+.heading {
+  font-size: 24px; /* Adjust the font size to your preference */
+  color: #333; /* Adjust the color to your preference */
+}
+.warning {
+  color: red;
+}
+
+button.custom-button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  padding: 10px;
+  border: none;
+  background-color: rgb(208, 245, 190);
+  border-radius: 12px;
+  color: black;
+  cursor: pointer;
+  transition: background-color 0.3s ease; /* Smooth transition */
+}
+
+button.custom-button:hover {
+  background-color: transparent; /* Remove background color on hover */
+}
+
 .container {
     display: flex;
     justify-content: center;
     align-items: center;
     height: 100vh;
     width: 190vh;
+    position: relative;
 }
 
 .divider {
@@ -68,6 +111,10 @@ export default {
     background-position: center;
     width: 100%;
     height: 100%;
+    background-repeat: no-repeat;
+    position: absolute;
+    top: 0;
+    left: 0;
 }
 
 .window {
@@ -82,6 +129,30 @@ export default {
     display: flex;
     justify-content: center;
     align-items: center;
+}
+
+@media (max-width: 768px) {
+    /* Adjust the content size for smaller screens */
+  .content {
+    font-size: 14px;
+  }
+  /* Adjust the window size for smaller screens */
+  .window {
+    width: 80%;
+    height: 60%;
+  }
+}
+
+@media (max-width: 480px) {
+    /* Further adjust the content size for even smaller screens */
+  .content {
+    font-size: 12px;
+  }
+  /* Further adjust the window size for even smaller screens */
+  .window {
+    width: 90%;
+    height: 70%;
+  }
 }
 
 .form-group {
